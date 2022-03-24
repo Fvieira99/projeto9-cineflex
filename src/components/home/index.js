b7871bd58e87d44b063ba3c09ec426d5113d3e15
Felipe Vieira <felipecosta1961@gmail.com>
2022-03-24 11:02:28 -0300

    This commit shows the film list on the home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,21 +1,36 @@
 import "./style.css";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 function Home() {
+	const [films, setFilms] = useState([]);
+
+	useEffect(() => {
+		const promise = axios.get(
+			"https://mock-api.driven.com.br/api/v5/cineflex/movies"
+		);
+		promise.then((response) => {
+			console.log(response);
+			const { data } = response;
+			setFilms(data);
+		});
+	}, []);
+
 	return (
 		<div className="Home">
 			<h2>Selecione o filme</h2>
 			<main>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
-				<p>filmes</p>
+				{films.map((film) => {
+					const { posterURL, id } = film;
+					return (
+						<Link key={id} to={`/sessoes/${id}`}>
+							<div className="film">
+								<img src={posterURL}></img>
+							</div>
+						</Link>
+					);
+				})}
 			</main>
 		</div>
 	);
--- src/components/sections/index.js
+++ src/components/sections/index.js
@@ -1,3 +1,23 @@
-function Sections() {}
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+function Sections() {
+	const [sections, setSections] = useState([]);
+	const { idFilme } = useParams;
+
+	useEffect(() => {
+		const promise = axios.get(
+			`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idFilme}/showtimes `
+		);
+		promise.then(() => {});
+	}, []);
+
+	return (
+		<div className="Sections">
+			<h2>Selecione a sessão</h2>
+		</div>
+	);
+}
 
 export default Sections;
